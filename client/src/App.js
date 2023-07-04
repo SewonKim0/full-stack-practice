3799ecd5285da9a5c04ed132b18148c1dcba94be
Sewon Kim <sewonkim0black@gmail.com>
2023-07-03 23:22:58 -0400

    server deleted, try again later

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  //state: data
+  const [data, setData] = useState(undefined);
+  //effect: fetch data
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        alert(json); //TEST
+        setData(json);
+      })
+  }, [])
+
   return (
     <div className="App">
-      <p> Hello World! Hello. </p>
+      <p> Hello World! Hello. The data is on the next line! </p>
+      {data === undefined ?
+      <p> Loading... </p> :
+      <ul>
+        {data.map((value) => {
+          return <li> {value} </li>;
+        })}  
+      </ul>}
     </div>
   );
 }
